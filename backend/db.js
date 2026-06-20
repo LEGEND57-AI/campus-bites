126f638226adffc7d46cb196ede9313308f85b70
LEGEND57-AI <godlike11381@gmail.com>
2026-06-20 22:04:53 +0530

    debug env variables

--- backend/db.js
+++ backend/db.js
@@ -3,6 +3,9 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+console.log("SUPABASE_URL =", process.env.SUPABASE_URL);
+console.log("SUPABASE_KEY EXISTS =", !!process.env.SUPABASE_SERVICE_ROLE_KEY);
+
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
